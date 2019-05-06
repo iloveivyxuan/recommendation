480e9f9183ed844021a8327f20022b9945778419
ivyxuan <iloveivyxuan@gmail.com>
2019-05-06 18:10:37 +0800

    Can calculate similarities

--- lib/recommendation/jaccard_input_matrix.rb
+++ lib/recommendation/jaccard_input_matrix.rb
@@ -14,12 +14,31 @@ module Recommendation
       end
       paris = get_all_pairs(item_ids)
       paris.each do |pair|
-        incr_items_both_views_count(pair)
+        incr_item_pair_views_count(pair)
       end
     end
 
+    def similarity_between(item1, item2)
+      val = item_pair_views_count(item1, item2)
+      val / (item_views_count(item1) + item_views_count(item2) - val).to_f
+    end
+
+    def similarities_for(item)
+      (all_items - [item]).map do |another_item|
+        [another_item, similarity_between(item, another_item)]
+      end
+    end
+
+    def item_pair_views_count(item1, item2)
+      Recommendation.redis.hget(redis_key(:item_pairs), [item1, item2].sort.join(':')).to_f
+    end
+
+    def item_views_count(item)
+      Recommendation.redis.hget(redis_key(:items), item).to_f
+    end
+
     def all_items
-      Recommendation.redis.hkeys(redis_key('items'))
+      Recommendation.redis.hkeys(redis_key(:items))
     end
 
     def get_all_pairs(item_ids)
@@ -31,7 +50,7 @@ module Recommendation
       Recommendation.redis.hincrby(redis_key(:items), item_id, 1)
     end
 
-    def incr_items_both_views_count(pair)
+    def incr_item_pair_views_count(pair)
       Recommendation.redis.hincrby(redis_key(:item_pairs), pair, 1)
     end
   end
--- spec/jaccard_input_matrix_spec.rb
+++ spec/jaccard_input_matrix_spec.rb
@@ -16,27 +16,58 @@ RSpec.describe Recommendation::JaccardInputMatrix, 'jaccard_input_matrix' do
 
   it 'should save views_count of a certain item in redis' do
     Recommendation.redis.hset('recommendation_test:views:items', 'article_1', 2)
-    @matrix.set_to_redis(%w[article_1 article_2])
+    @matrix.count_items_views_in_redis(%w[article_1 article_2])
     expect(Recommendation.redis.hget('recommendation_test:views:items', 'article_1')).to eq('3')
     expect(Recommendation.redis.hget('recommendation_test:views:items', 'article_2')).to eq('1')
   end
 
   it 'should save views_count of certain pair of items in redis' do
     Recommendation.redis.hset('recommendation_test:views:item_pairs', 'article_1:article_2', 2)
-    @matrix.set_to_redis(%w[article_1 article_2])
+    @matrix.count_items_views_in_redis(%w[article_1 article_2])
     expect(
       Recommendation.redis.hget('recommendation_test:views:item_pairs', 'article_1:article_2')
     ).to eq('3')
   end
 
   it 'should return all items as an array' do
-    @matrix.set_to_redis(%w[article_1 article_2])
-    @matrix.set_to_redis(%w[article_1 article_3])
+    @matrix.count_items_views_in_redis(%w[article_1 article_2])
+    @matrix.count_items_views_in_redis(%w[article_1 article_3])
     expect(@matrix.all_items).to eq(%w[article_1 article_2 article_3])
   end
 
   it 'should get all pairs of tiems viewed by one user' do
     pairs = @matrix.get_all_pairs(%w[article_1 article_2 article_3])
     expect(pairs.count).to eq(3)
+  end
+
+  it 'should calculate the correct similarity between two items' do
+    add_two_item_test_data!(@matrix)
+    expect(@matrix.similarity_between('article_4', 'article_5')).to eq(0.4)
+    expect(@matrix.similarity_between('article_5', 'article_4')).to eq(0.4)
+  end
+
+  it 'should calculate all similarities of a certain item to other items' do
+    add_three_item_test_data!(@matrix)
+    result = @matrix.similarities_for('article_4')
+    expect(result).to include(['article_5', 0.4])
+    expect(result).to include(['article_6', 0.75])
+  end
+
+  private
+
+  def add_two_item_test_data!(matrix)
+    matrix.count_items_views_in_redis(%w[article_4 article_5])
+    matrix.count_items_views_in_redis(%w[article_5])
+    matrix.count_items_views_in_redis(%w[article_4])
+    matrix.count_items_views_in_redis(%w[article_4 article_5])
+    matrix.count_items_views_in_redis(%w[article_4])
+  end
+
+  def add_three_item_test_data!(matrix)
+    matrix.count_items_views_in_redis(%w[article_4 article_5 article_6])
+    matrix.count_items_views_in_redis(%w[article_5])
+    matrix.count_items_views_in_redis(%w[article_4 article_6])
+    matrix.count_items_views_in_redis(%w[article_4 article_5])
+    matrix.count_items_views_in_redis(%w[article_4 article_6])
   end
 end
